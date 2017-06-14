a6b368a3abb354a949db6717a279ac1c330dfb84
Asger Bachmann <asger.g.bachmann@jp.dk>
2017-06-14 10:31:26 +0200

    Error handling for the replicator lambda.

--- stream-replicator/lambda_stream_replicator.py
+++ stream-replicator/lambda_stream_replicator.py
@@ -1,7 +1,7 @@
 """
 Takes an input stream and replicates a fraction of Snowplow records to an output stream, changing the records' app_id in the process.
 """
-import base64, json, boto3, sys, os
+import base64, boto3, sys, os
 
 keep_one_in_X_events = 100
 out_stream_name = os.environ["ENV_OUTPUT_STREAM_NAME"]
@@ -18,7 +18,7 @@ def handler(event, context):
             continue
 
         try:
-            payload = json.loads(base64.b64decode(record["kinesis"]["data"]))
+            payload = base64.b64decode(record["kinesis"]["data"]).decode("UTF-8")
 
             # Replace the app_id with a string containing the old app_id as well.
             index_of_first_tab = payload.find("\t")
@@ -27,13 +27,13 @@ def handler(event, context):
             # To easily support filtering of Kinesis events, we write the target environment first as part of the app_id.
             new_app_id = "dev.lambda_replicate_from_prod ({})".format(orig_app_id)
             new_payload = new_app_id + payload_excl_app_id
-            records.push({"Data": bytes(new_payload, "UTF-8"), "PartitionKey": seq_number})
+            records.append({"Data": bytes(new_payload, "UTF-8"), "PartitionKey": seq_number})
         except Exception as e:
             print("ERROR: {}\nInput: {}".format(str(e), record["kinesis"]["data"]), file=sys.stderr)
 
     if len(records) == 0:
         return
-        
+
     sts_client = boto3.client("sts")
     sts_response = sts_client.assume_role(RoleArn=assume_role_arn, RoleSessionName=assume_role_session_name, DurationSeconds=900)
 
