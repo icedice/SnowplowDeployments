2d45c2c21b33f1c15d96e683a73ad22953bcd0e3
Asger Bachmann <asger.g.bachmann@jp.dk>
2018-08-23 22:49:19 +0200

    Add script to move data to DFP.

---
+++ snowplow_tsv_to_parquet/move_to_dfp/README.md
@@ -0,0 +1,36 @@
+# Transfer data to Dataforsyningsplatform (DFP)
+Currently we only transfer pageviews to DFP. This is triggered by the same Airflow DAG that triggers the parent `snowplow_tsv_to_parquet` project, except that this job is only triggered every midnight instead of every hour.
+
+# Developing locally
+Create virtual environment (or do it your preferred way):
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Set your environment variables for the current shell:
+```bash
+export S3_INPUT_BUCKET=behavior-datalake-test
+export S3_INPUT_PREFIX=snowplow/event=page_view
+export S3_OUTPUT_BUCKET=behavior-datalake-test
+export S3_OUTPUT_PREFIX=snowplow_pageviews_dfp
+```
+
+Run the script for a given date:
+```bash
+python3 move_to_dfp.py 2018-08-22
+```
+
+__CAUTION__: If you run the script multiple times for the same date, multiple files with duplicate data will be created so be weary of this in prod.
+
+## Cleanup in S3
+```bash
+aws s3 rm --recursive s3://${S3_OUTPUT_BUCKET}/${S3_OUTPUT_PREFIX}
+```
+
+# Building and running the Docker image
+```bash
+docker build -t move_to_dfp .
+docker run -it -e AWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID' -e AWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY' -e AWS_SESSION_TOKEN='$AWS_SESSION_TOKEN' move_to_dfp 2018-08-22
+```
---
+++ snowplow_tsv_to_parquet/move_to_dfp/move_to_dfp.py
@@ -0,0 +1,125 @@
+import logging
+import sys
+import time
+import os
+from datetime import date, datetime
+
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from s3fs import S3FileSystem
+
+
+S3_INPUT_BUCKET = os.environ['S3_INPUT_BUCKET']  # 'behavior-datalake' on prod.
+S3_INPUT_PREFIX = os.environ['S3_INPUT_PREFIX']  # 'snowplow/event=page_view' on prod
+S3_OUTPUT_BUCKET = os.environ['S3_OUTPUT_BUCKET']  # 'jyllandsposten-upload-prod' on prod
+S3_OUTPUT_PREFIX = os.environ['S3_OUTPUT_PREFIX']  # 'snowplow_pageviews' on prod
+
+
+def add_dt_cols(date_to_process: date, table: pa.Table) -> pa.Table:
+    def str_zfill(val: int, zfill: int):
+        return pd.Series(str(val), index=range(len(table))).str.zfill(zfill)
+
+    year = str_zfill(date_to_process.year, 4)
+    month = str_zfill(date_to_process.month, 2)
+    day = str_zfill(date_to_process.day, 2)
+    dt = pd.Series(date_to_process.isoformat(), index=range(len(table)))
+
+    return table\
+        .append_column(pa.Column.from_array('year', year))\
+        .append_column(pa.Column.from_array('month', month))\
+        .append_column(pa.Column.from_array('day', day))\
+        .append_column(pa.Column.from_array('dt', dt))
+
+
+def add_brand_col(table: pa.Table) -> pa.Table:
+    brand = pd.Categorical(np.where(table.column('site').to_pandas() == 'jyllands-posten.dk', 'jp', 'erhvervsmedier'))
+    return table.append_column(pa.Column.from_array('brand', brand))
+
+
+def convert_tstamps(table: pa.Table) -> pa.Table:
+    # Some tstamps must be converted to ms (instead of ns) to avoid errors from Arrow.
+    tstamps_to_convert = ['refr_device_tstamp', 'true_tstamp']
+    new_tstamps = [table.column(c).to_pandas().astype('datetime64[ms]') for c in tstamps_to_convert]
+
+    table = table.drop(tstamps_to_convert)
+    # Add all the new tstamp columns.
+    for (col_name, new_col) in zip(tstamps_to_convert, new_tstamps):
+        table = table.append_column(pa.Column.from_array(col_name, new_col))
+
+    return table
+
+
+def drop_contexts(table: pa.Table) -> pa.Table:
+    # 'contexts' should be dropped entirely because it contains sensitive information (namely SSOid) embedded in the
+    # JSON which is somewhat difficult for the DFP to hash.
+    return table.drop(['contexts'])
+
+
+def read_table(date_to_process: date, read_nthreads: int, fs: S3FileSystem) -> pa.Table:
+    in_date_path = date_to_process.strftime('year=%Y/month=%m/day=%d')
+    in_path = 's3://{}/{}/{}'.format(S3_INPUT_BUCKET, S3_INPUT_PREFIX, in_date_path)
+    logging.info('Reading data for input path {}.'.format(in_path))
+
+    try:
+        ds = pq.ParquetDataset(in_path, filesystem=fs, metadata_nthreads=read_nthreads)
+    except OSError as ex:
+        logging.exception('Unexpected error occured when reading data.' +
+                          ' Make sure the path {} exists and that you have access to it.'.format(in_path))
+        exit(1)
+
+    return ds.read()
+
+
+def write_dataset(table: pa.Table, fs: S3FileSystem) -> None:
+    partitions = ['brand', 'year', 'month', 'dt']
+    out_path = 's3://{}/{}'.format(S3_OUTPUT_BUCKET, S3_OUTPUT_PREFIX)
+
+    logging.info('Writing data to {}...'.format(out_path))
+
+    # Note that PyArrow does not support overwriting data so if you run this multiple times, it will create multiple files
+    # for the same paritions.
+    pq.write_to_dataset(table, root_path=out_path, filesystem=fs, partition_cols=partitions)
+
+
+def main(date_to_process: date, fs: S3FileSystem, read_nthreads: int):
+    logging.info('Reading data for {}...'.format(date_to_process.isoformat()))
+    table = read_table(date_to_process, read_nthreads, fs)
+
+    logging.info('Dropping contexts...')
+    table = drop_contexts(table)
+
+    logging.info('Converting timestamps...')
+    table = convert_tstamps(table)
+
+    logging.info('Adding brand...')
+    table = add_brand_col(table)
+
+    logging.info('Adding date columns for use in partitioning...')
+    table = add_dt_cols(date_to_process, table)
+
+    logging.info('Writing output...')
+    write_dataset(table, fs)
+
+    logging.info('Done.')
+
+
+if __name__ == '__main__':
+    logging.Formatter.converter = time.gmtime
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+
+    if len(sys.argv) < 2:
+        logging.error('Need one argument: a date in format YYYY-mm-dd.')
+        exit(1)
+
+    date_to_process_str = sys.argv[1]
+    try:
+        date_to_process = datetime.strptime(date_to_process_str, "%Y-%m-%d").date()
+    except ValueError as ex:
+        logging.exception('Could not parse input date {} with format YYYY-mm-dd.'.format(date_to_process_str))
+
+    # Construct a S3 connection using the default credentials provider chain from boto3. The ACL is required for DFP to
+    # own the files when uploading them.
+    fs = S3FileSystem(s3_additional_kwargs={'ACL': 'bucket-owner-full-control'})
+    main(date_to_process, fs, read_nthreads=8)
