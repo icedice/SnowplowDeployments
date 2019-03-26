7b168ae119b5a6bbbf34bd174284d75e82921ef4
Asger Bachmann <asger.g.bachmann@jp.dk>
2019-03-26 09:44:59 +0100

    SP-to-DFP now triggered every hour and with correct output.

--- snowplow_tsv_to_parquet/move_to_dfp/README.md
+++ snowplow_tsv_to_parquet/move_to_dfp/README.md
@@ -1,5 +1,5 @@
 # Transfer data to Dataforsyningsplatform (DFP)
-Currently we only transfer pageviews to DFP. This is triggered by the same Airflow DAG that triggers the parent `snowplow_tsv_to_parquet` project, except that this job is only triggered every midnight instead of every hour.
+Currently we only transfer pageviews to DFP. This is triggered by the same Airflow DAG that triggers the parent `snowplow_tsv_to_parquet` project.
 
 # Developing locally
 Create virtual environment (or do it your preferred way):
@@ -16,19 +16,21 @@ export S3_OUTPUT_BUCKET=behavior-datalake-test
 export S3_OUTPUT_PREFIX=snowplow_pageviews_dfp
 ```
 
-Run the script for a given date:
+Run the script for a given date and hour:
 ```bash
-python3 move_to_dfp.py 2018-08-22
+python3 move_to_dfp.py 2018-08-22 10
 ```
 
 __CAUTION__: If you run the script multiple times for the same date, multiple files with duplicate data will be created so be weary of this in prod.
 
 ## Cleanup in S3
+__USE WITH CAUTION__
 ```bash
 aws s3 rm --recursive s3://${S3_OUTPUT_BUCKET}/${S3_OUTPUT_PREFIX}
 ```
 
 # Building and running the Docker image
+Assuming AWS id, key and token is set as environment variables (you can use the `with_aws_env` script for this).
 ```bash
 docker build -t move_to_dfp .
 docker run -it -e AWS_ACCESS_KEY_ID='$AWS_ACCESS_KEY_ID' -e AWS_SECRET_ACCESS_KEY='$AWS_SECRET_ACCESS_KEY' -e AWS_SESSION_TOKEN='$AWS_SESSION_TOKEN' move_to_dfp 2018-08-22
--- snowplow_tsv_to_parquet/move_to_dfp/move_to_dfp.py
+++ snowplow_tsv_to_parquet/move_to_dfp/move_to_dfp.py
@@ -16,19 +16,21 @@ S3_OUTPUT_BUCKET = os.environ['S3_OUTPUT_BUCKET']  # 'jyllandsposten-upload-prod
 S3_OUTPUT_PREFIX = os.environ['S3_OUTPUT_PREFIX']  # 'snowplow_pageviews' on prod
 
 
-def add_dt_cols(date_to_process: date, table: pa.Table) -> pa.Table:
+def add_dt_cols(date_to_process: date, hour_to_process: int, table: pa.Table) -> pa.Table:
     def str_zfill(val: int, zfill: int):
         return pd.Series(str(val), index=range(len(table))).str.zfill(zfill)
 
     year = str_zfill(date_to_process.year, 4)
     month = str_zfill(date_to_process.month, 2)
     day = str_zfill(date_to_process.day, 2)
+    hour = str_zfill(hour_to_process, 2)
     dt = pd.Series(date_to_process.isoformat(), index=range(len(table)))
 
     return table\
         .append_column(pa.Column.from_array('year', year))\
         .append_column(pa.Column.from_array('month', month))\
         .append_column(pa.Column.from_array('day', day))\
+        .append_column(pa.Column.from_array('hour', hour))\
         .append_column(pa.Column.from_array('dt', dt))
 
 
@@ -49,22 +51,34 @@ def convert_tstamps(table: pa.Table) -> pa.Table:
 
     return table
 
+"""
+Drop non-whitelisted columns that DFP are not interested in to keep the output as simple as possible.
 
-def drop_contexts(table: pa.Table) -> pa.Table:
-    # 'contexts' should be dropped entirely because it contains sensitive information (namely SSOid) embedded in the
-    # JSON which is somewhat difficult for the DFP to hash.
-    return table.drop(['contexts'])
+If you extend this whitelist, make sure not to include the context column as it contains sensitive information (namely 
+# SSOid) embedded in the JSON which is somewhat difficult for the DFP to hash.
+"""
+def drop_columns(table: pa.Table) -> pa.Table:
+    col_whitelist = set(['collector_tstamp', 'event_type', 'user_ipaddress', 'network_id', 'geo_country', 'geo_city',
+     'geo_region_name', 'geo_zipcode', 'page_url', 'page_title', 'page_referrer', 'page_urlscheme', 'page_urlhost',
+     'page_urlpath', 'page_urlquery', 'page_urlfragment', 'refr_urlscheme', 'refr_urlhost', 'refr_urlpath',
+     'refr_urlquery', 'refr_urlfragment', 'refr_medium', 'refr_source', 'refr_term', 'mkt_medium', 'mkt_source',
+     'mkt_term', 'mkt_content', 'mkt_campaign', 'mkt_clickid', 'mkt_network', 'useragent', 'br_name', 'br_family',
+     'br_version', 'os_name', 'os_family', 'dvce_type', 'domain_sessionid', 'derived_tstamp', 'anon_id', 'user_id',
+     'user_authorized', 'grp_authorized', 'site', 'content_id', 'section_id', 'page_restricted', 'web_page_id'])
+
+    to_drop = set(c.name for c in table.columns) - col_whitelist
+    return table.drop(to_drop)
 
 
-def read_table(date_to_process: date, read_nthreads: int, fs: S3FileSystem) -> pa.Table:
-    in_path = 's3://{}/snowplow/event=page_view/date={}'.format(S3_INPUT_BUCKET, str(date_to_process))
-    logging.info('Reading data for input path {}.'.format(in_path))
+def read_table(date_to_process: date, hour_to_process: int, read_nthreads: int, fs: S3FileSystem) -> pa.Table:
+    in_path = f's3://{S3_INPUT_BUCKET}/snowplow/event=page_view/date={date_to_process}/hour={hour_to_process:02d}'
+    logging.info(f'Reading data for input path {in_path}.')
 
     try:
         ds = pq.ParquetDataset(in_path, filesystem=fs, metadata_nthreads=read_nthreads)
     except OSError as ex:
-        logging.exception('Unexpected error occured when reading data.' +
-                          ' Make sure the path {} exists and that you have access to it.'.format(in_path))
+        logging.exception(f'Unexpected error occured when reading data.' +
+                           ' Make sure the path {in_path} exists and that you have access to it.')
         exit(1)
 
     return ds.read()
@@ -72,30 +86,30 @@ def read_table(date_to_process: date, read_nthreads: int, fs: S3FileSystem) -> p
 
 def write_dataset(table: pa.Table, fs: S3FileSystem) -> None:
     partitions = ['brand', 'year', 'month', 'dt']
-    out_path = 's3://{}/{}'.format(S3_OUTPUT_BUCKET, S3_OUTPUT_PREFIX)
+    out_path = f's3://{S3_OUTPUT_BUCKET}/{S3_OUTPUT_PREFIX}'
 
-    logging.info('Writing data to {}...'.format(out_path))
+    logging.info(f'Writing data to {out_path}...')
 
     # Note that PyArrow does not support overwriting data so if you run this multiple times, it will create multiple files
     # for the same paritions.
     pq.write_to_dataset(table, root_path=out_path, filesystem=fs, partition_cols=partitions)
 
 
-def main(date_to_process: date, fs: S3FileSystem, read_nthreads: int):
-    logging.info('Reading data for {}...'.format(date_to_process.isoformat()))
-    table = read_table(date_to_process, read_nthreads, fs)
-
-    logging.info('Dropping contexts...')
-    table = drop_contexts(table)
+def main(date_to_process: date, hour_to_process: int, fs: S3FileSystem, read_nthreads: int):
+    logging.info(f'Reading data for {date_to_process.isoformat()} {hour_to_process:02d}...')
+    table = read_table(date_to_process, hour_to_process, read_nthreads, fs)
 
     logging.info('Converting timestamps...')
     table = convert_tstamps(table)
+
+    logging.info('Dropping columns...')
+    table = drop_columns(table)
 
     logging.info('Adding brand...')
     table = add_brand_col(table)
 
     logging.info('Adding date columns for use in partitioning...')
-    table = add_dt_cols(date_to_process, table)
+    table = add_dt_cols(date_to_process, hour_to_process, table)
 
     logging.info('Writing output...')
     write_dataset(table, fs)
@@ -107,17 +121,27 @@ if __name__ == '__main__':
     logging.Formatter.converter = time.gmtime
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
-    if len(sys.argv) < 2:
-        logging.error('Need one argument: a date in format YYYY-mm-dd.')
+    if len(sys.argv) < 3:
+        logging.error('Need two arguments: 1) a date in format YYYY-mm-dd, 2) hour in the format HH.')
         exit(1)
 
     date_to_process_str = sys.argv[1]
     try:
         date_to_process = datetime.strptime(date_to_process_str, "%Y-%m-%d").date()
     except ValueError as ex:
-        logging.exception('Could not parse input date {} with format YYYY-mm-dd.'.format(date_to_process_str))
+        logging.exception(f'Could not parse input date {date_to_process_str} with format YYYY-mm-dd.')
+        exit(1)
+
+    hour_to_process_str = sys.argv[2]
+    try:
+        hour_to_process = int(hour_to_process_str)
+        if 0 < hour_to_process > 23:
+            raise ValueError()
+    except ValueError as ex:
+        logging.exception(f'Could not parse input hour {hour_to_process_str} with format HH.')
+        exit(1)
 
     # Construct a S3 connection using the default credentials provider chain from boto3. The ACL is required for DFP to
     # own the files when uploading them.
     fs = S3FileSystem(s3_additional_kwargs={'ACL': 'bucket-owner-full-control'})
-    main(date_to_process, fs, read_nthreads=8)
+    main(date_to_process, hour_to_process, fs, read_nthreads=8)
