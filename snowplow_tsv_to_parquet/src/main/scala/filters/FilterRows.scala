acdf09d297223b205b855e6d057268d3136177d9
signewp <signe.s.w.petersen@jp.dk>
2019-03-27 13:28:52 +0100

    Tsv-to-parquet: Cleaned up FilterRows

--- snowplow_tsv_to_parquet/src/main/scala/filters/FilterRows.scala
+++ snowplow_tsv_to_parquet/src/main/scala/filters/FilterRows.scala
@@ -2,10 +2,9 @@ package filters
 
 import dk.jp.snowplow_tsv_to_parquet.util.Schemas
 
-/***
-  Object to filter rows for the events. Currently, rows with user agent containing "CookieInformationScanner" is removed.
-  ***/
-
+/**
+  * Object to filter rows for the events. Currently, rows with user agent containing "CookieInformationScanner" is removed.
+  */
 object FilterRows {
 
   def filterRows(in: Iterator[Array[Any]]): Iterator[Array[Any]] = {
@@ -13,10 +12,10 @@ object FilterRows {
   }
 
   private def shouldRemoveBasedOnUserAgent(event: Array[Any]): Boolean = {
-    val x: Any = Option(event(Schemas.inFieldNameToIdx("useragent")))
-    x match {
-      case Some(value: String) => value.contains("CookieInformationScanner")
+    Option(event(Schemas.inFieldNameToIdx("useragent"))) match {
+      case Some(userAgent: String) => userAgent.endsWith("CookieInformationScanner")
       case _ => false
     }
   }
+
 }
